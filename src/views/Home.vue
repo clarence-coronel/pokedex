6f7776103998d6c2cde3661b61d67d4fd5b35037
Clarence-Coronel <clarence.coronel.r@gmail.com>
2024-03-09 22:19:02 +0800

    Add fetching of data from pokeapi with next and prev

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,43 @@
 <template>
+    <div class="flex justify-center items-center flex-col gap-5 bg-red-400 h-screen overflow-y-auto">
+      <div v-for="poke, index in pokemons" :key="index" class="font-semibold text-xl">
+        {{ poke.name }}
+      </div>
+  
+      <button @click="() => fetchData(prev)">Prev</button>
+      <button @click="() => fetchData(link)">Next</button>
+    </div>
+  </template>
+  
+  <script setup>
+  import { onMounted, ref } from 'vue';
+  
+  const pokemons = ref([]);
+  const link = ref('https://pokeapi.co/api/v2/pokemon');
+  const prev = ref('https://pokeapi.co/api/v2/pokemon');
+  
+  async function fetchData(url) {
+    try {
+      const res = await fetch(url);
+  
+      if (!res.ok) {
+        throw new Error('Not Ok');
+      }
+  
+      const data = await res.json();
+      
+      pokemons.value = data.results;
+      
+      if(data.previous) prev.value = data.previous;
+      link.value = data.next;
 
-</template>
-
-<script setup>
-
-</script>
+    } catch (error) {
+      console.error(error);
+    }
+  }
+  
+  onMounted(() => {
+    fetchData(link.value);
+  });
+  </script>
+  
