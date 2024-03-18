96e47f74332147604d310ae42d4375a155677d4e
Clarence-Coronel <clarence.coronel.r@gmail.com>
2024-03-18 20:32:57 +0800

    save

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="w-full min-h-screen flex justify-start items-center flex-col gap-5 p-5">
+  <div class="w-full min-h-screen flex justify-start items-center flex-col gap-5 p-5  ">
     <div class="text-red-400 text-5xl font-bold font-K2D flex flex-col flex-nowrap gap-2 justify-center items-center sm:flex-row">
       <!-- <img class="animate-pulse  w-20" src="https://videogames.si.com/.image/t_share/MjAxNDY1NDMyNzIzNDMzMDEz/poke-ball.png" alt=""> -->
       <!-- <svg xmlns="http://www.w3.org/2000/svg" width="4rem" height="4rem" viewBox="0 0 24 24"><path fill="currentColor" d="M14.5 12a2.5 2.5 0 0 1-5 0a2.5 2.5 0 0 1 5 0m7.5 0c0 5.52-4.48 10-10 10S2 17.52 2 12S6.48 2 12 2s10 4.48 10 10m-2 0h-4c0-2.21-1.79-4-4-4s-4 1.79-4 4H4c0 4.41 3.59 8 8 8s8-3.59 8-8"/></svg> -->
