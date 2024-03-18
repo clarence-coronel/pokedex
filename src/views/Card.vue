238b4478a12032bf58e6feb9b7188016a96e77bc
Clarence-Coronel <clarence.coronell@gmail.com>
2024-03-18 17:52:56 +0800

    Add new loading anim to cards and new header

--- src/views/Card.vue
+++ src/views/Card.vue
@@ -14,8 +14,18 @@
             </div>
         </div>
     </router-link>
-    <div v-else-if="delayFinished && isLoading" class="aspect-square flex flex-col justify-center items-center p-5 rounded-lg bg-white shadow-center-sm">
-        <svg class="text-red-400 animate-spin" xmlns="http://www.w3.org/2000/svg" width="4rem" height="4rem" viewBox="0 0 24 24"><path fill="currentColor" d="M14.5 12a2.5 2.5 0 0 1-5 0a2.5 2.5 0 0 1 5 0m7.5 0c0 5.52-4.48 10-10 10S2 17.52 2 12S6.48 2 12 2s10 4.48 10 10m-2 0h-4c0-2.21-1.79-4-4-4s-4 1.79-4 4H4c0 4.41 3.59 8 8 8s8-3.59 8-8"/></svg>
+    <div v-else-if="delayFinished && isLoading" class="gap-3 animate-pulse aspect-square flex flex-col justify-center items-center p-5 rounded-lg bg-white shadow-center-sm">
+        <div class="w-full aspect-square bg-neutral-200 rounded-md flex items-center justify-center">
+        </div>
+
+        <div class="w-full h-10 bg-neutral-200 rounded-md text-center flex justify-center items-center text-xl font-medium">
+        </div>
+
+        <div class="w-full h-fit flex flex-wrap gap-2 justify-center">
+            <span class="w-10 h-5 bg-neutral-200 px-2 py-1 text-white rounded-md"></span>
+            <span class="w-10 h-5 bg-neutral-200 px-2 py-1 text-white rounded-md"></span>
+            <span class="w-10 h-5 bg-neutral-200 px-2 py-1 text-white rounded-md"></span>
+        </div>
     </div>
 </template>
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,14 +3,15 @@
     <div class="text-red-400 text-5xl font-bold font-K2D flex flex-col flex-nowrap gap-2 justify-center items-center sm:flex-row">
       <!-- <img class="animate-pulse  w-20" src="https://videogames.si.com/.image/t_share/MjAxNDY1NDMyNzIzNDMzMDEz/poke-ball.png" alt=""> -->
       <!-- <svg xmlns="http://www.w3.org/2000/svg" width="4rem" height="4rem" viewBox="0 0 24 24"><path fill="currentColor" d="M14.5 12a2.5 2.5 0 0 1-5 0a2.5 2.5 0 0 1 5 0m7.5 0c0 5.52-4.48 10-10 10S2 17.52 2 12S6.48 2 12 2s10 4.48 10 10m-2 0h-4c0-2.21-1.79-4-4-4s-4 1.79-4 4H4c0 4.41 3.59 8 8 8s8-3.59 8-8"/></svg> -->
-      <span>Pokédex</span>
+      <!-- <span>Pokédex</span> -->
+      <img class="w-64" src="https://archives.bulbagarden.net/media/upload/4/4b/Pok%C3%A9dex_logo.png" alt="">
     </div>
 
     <div v-if="!isLoading" class="w-full grid grid-cols-[repeat(auto-fill,minmax(200px,1fr))] gap-10 p-5">
        <Card  v-for="poke in pokemonList" :key="poke.name" :name="poke.name" :dataUrl="poke.url" /> 
     </div>
-    <div v-else-if="isLoading && delayFinished" class="flex items-start justify-center pt-40 animate-pulse text-red-400 font-semibold text-3xl h-[70vh]">
-        Loading...
+    <div v-else-if="delayFinished && isLoading" class="w-full min-h-[70vh] flex flex-col justify-center items-center p-5 rounded-lg bg-white">
+        <svg class="text-red-400 animate-spin" xmlns="http://www.w3.org/2000/svg" width="4rem" height="4rem" viewBox="0 0 24 24"><path fill="currentColor" d="M14.5 12a2.5 2.5 0 0 1-5 0a2.5 2.5 0 0 1 5 0m7.5 0c0 5.52-4.48 10-10 10S2 17.52 2 12S6.48 2 12 2s10 4.48 10 10m-2 0h-4c0-2.21-1.79-4-4-4s-4 1.79-4 4H4c0 4.41 3.59 8 8 8s8-3.59 8-8"/></svg>
     </div>
 
     <div class="flex flex-col-reverse w-full gap-3 sm:flex-row sm:gap-5 justify-center items-center">
